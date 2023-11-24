da1e8410bc3655e48108026199fc2b6b60664564
Haakon Webb <haakonwebb@gmail.com>
2023-11-24 12:26:53 +0900

    Implement Data Processing Stage Tracking
    Added data_processing_tracker.py to handle metadata updates and stage checking for data files.
    
    This module includes functions to dynamically update file metadata based on the processing stage and to verify if a specific processing stage has been completed, ensuring streamlined and accurate data flow through preprocessing, cleaning, and normalization stages.

---
+++ src/data_processing_tracker.py
@@ -0,0 +1,46 @@
+# Copyright (C) 2023 Haakon Vollheim Webb
+# This file is part of NEPP which is released under GNU GPLv3.
+# See file LICENSE or go to https://www.gnu.org/licenses/gpl-3.0.html for full license details.
+
+
+def update_file_metadata(file_path, new_stage):
+    """
+    Update the metadata of a file to include a new processing stage.
+
+    Parameters:
+    file_path (str): Path to the file.
+    new_stage (str): The processing stage to add (e.g., 'preprocessed').
+    """
+    try:
+        with open(file_path, 'r+') as file:
+            content = file.readlines()
+            if len(content) > 0:
+                stages = content[0].strip().split(',')
+                if new_stage not in stages:
+                    stages.append(new_stage)
+                    content[0] = ','.join(stages) + '\n'
+            else:
+                content.insert(0, new_stage + '\n')
+            file.seek(0)
+            file.writelines(content)
+    except IOError as e:
+        print(f"Error updating file metadata: {e}")
+
+def check_processing_stage(file_path, required_stage):
+    """
+    Check if a file has undergone a specific processing stage.
+
+    Parameters:
+    file_path (str): Path to the file.
+    required_stage (str): The processing stage to check (e.g., 'cleaned').
+
+    Returns:
+    bool: True if the stage is present, False otherwise.
+    """
+    try:
+        with open(file_path, 'r') as file:
+            first_line = file.readline().strip()
+            return required_stage in first_line.split(',')
+    except IOError as e:
+        print(f"Error reading file metadata: {e}")
+        return False
