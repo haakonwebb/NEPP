302894eded6cf769334d78af85617cb9d7fdd3fc
Haakon Webb <haakonwebb@gmail.com>
2023-11-24 12:41:29 +0900

    Add Initial Project Configuration Settings
    Introduced config.py with basic placeholder settings for data directories, model saving path, and training parameters. These settings provide a starting point for the project's configuration and are expected to evolve as the project develops.

--- src/config.py
+++ src/config.py
@@ -2,3 +2,10 @@
 # This file is part of NEPP which is released under GNU GPLv3.
 # See file LICENSE or go to https://www.gnu.org/licenses/gpl-3.0.html for full license details.
 
+# Configuration settings for the project
+
+DATA_RAW_DIR = 'data/raw/'
+DATA_PROCESSED_DIR = 'data/processed/'
+MODEL_SAVE_PATH = 'models/trained_model.h5'
+TRAINING_EPOCHS = 100
+BATCH_SIZE = 32
