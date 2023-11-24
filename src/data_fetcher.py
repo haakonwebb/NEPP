b97a26c222fa5bca609cae759c37b3ac0cb1306b
Haakon Webb <haakonwebb@gmail.com>
2023-11-24 12:36:08 +0900

    Refactor and Enhance Data Fetching in data_fetcher.py
    Changed area code representation from individual variables to a dictionary for improved scalability and organization.
    
    Added align_to_monday function for date alignment to week's start.
    
    Modified fetch_data function to accept area code names, fetch data weekly, and save with informative filenames including week numbers and date ranges.
    
    Updated example usage to demonstrate new fetching strategy and parameters.

--- src/data_fetcher.py
+++ src/data_fetcher.py
@@ -13,33 +13,62 @@ load_dotenv()
 # Get the API key from the environment variable
 api_key = os.getenv("ENTSOE_API_KEY")
 
-# Norwegian bidding zones as individual variables
-NO1 = "10YNO-1--------2"  # NO1 Eastern Norway
-NO2 = "10YNO-2--------T"  # NO2 Southern Norway
-NO3 = "10YNO-3--------J"  # NO3 Central Norway
-NO4 = "10YNO-4--------9"  # NO4 Northern Norway
-NO5 = "10Y1001A1001A48H"  # NO5 Western Norway
+# Norwegian bidding zones as a tuple
+area_codes = {
+    "NO1": "10YNO-1--------2",  # NO1 Eastern Norway
+    "NO2": "10YNO-2--------T",  # NO2 Southern Norway
+    "NO3": "10YNO-3--------J",  # NO3 Central Norway
+    "NO4": "10YNO-4--------9",  # NO4 Northern Norway
+    "NO5": "10Y1001A1001A48H"   # NO5 Western Norway
+}
+
 
 client = EntsoeRawClient(api_key=api_key)
 
-def fetch_data(start_date, end_date, country_code):
+def align_to_monday(date):
+    """
+    Adjusts a given date to the previous Monday if it's not already a Monday.
+
+    Parameters:
+    date (pd.Timestamp): The date to align.
+
+    Returns:
+    pd.Timestamp: The aligned date.
+    """
+    return date - pd.DateOffset(days=date.weekday())
+
+def fetch_data(start_date, end_date, area_code_name):
+    country_code = area_codes.get(area_code_name)
+    if not country_code:
+        print(f"Invalid area code name: {area_code_name}")
+        return
+
     start = pd.Timestamp(start_date, tz='Europe/Oslo')
     end = pd.Timestamp(end_date, tz='Europe/Oslo')
-    xml_string = client.query_day_ahead_prices(country_code, start, end)
+    current = align_to_monday(start)
 
-    # Directory path for raw data
-    raw_data_directory = os.path.join('data', 'raw')
+    while current < end:
+        # Set the end of the week to the nearest Sunday, including all of Sunday
+        week_end = min(current + pd.DateOffset(days=6, hours=23, minutes=59, seconds=59), end)
+        print(f"Fetching data from {current} to {week_end}...")
 
-    # Ensure the directory exists
-    os.makedirs(raw_data_directory, exist_ok=True)
+        xml_string = client.query_day_ahead_prices(country_code, current, week_end)
 
-    # Define the path for the output file
-    output_file_path = os.path.join(raw_data_directory, f'{country_code}_{start_date}_to_{end_date}_prices.xml')
+        # Save each week's data in a separate file
+        raw_data_directory = os.path.join('data', 'raw')
+        os.makedirs(raw_data_directory, exist_ok=True)
 
-    # Write the data to the file
-    with open(output_file_path, 'w') as f:
-        f.write(xml_string)
+        # Format the filename with the year, month, and week number
+        filename = f"{area_code_name}_Week{current.strftime('%W')}_{current.strftime('%Y_%m_%d')}_to_{week_end.strftime('%Y_%m_%d')}_prices.xml"
+        output_file_path = os.path.join(raw_data_directory, filename)
+
+        with open(output_file_path, 'w') as f:
+            f.write(xml_string)
+
+        print(f"Data saved to {output_file_path}")
+        # Move to the next Monday
+        current = week_end + pd.DateOffset(minutes=1) if week_end < end else end
 
 if __name__ == "__main__":
-    # Example usage:
-    fetch_data('20231102', '20231103', NO5)
+    # Example usage
+    fetch_data('20231002', '20231102', 'NO1')
