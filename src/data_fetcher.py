0ec704ba2d6e12232a10cd1b9e316c06da722ce1
Haakon Webb <haakonwebb@gmail.com>
2023-11-27 19:09:22 +0900

    Update data_fetcher.py for efficient and resilient data fetching - Implemented fetch_data_with_retries for robust error handling and retry logic - Revised fetch_data to accommodate larger date ranges by splitting requests - Added exception handling for ConnectionError, Timeout, and TooManyRedirects - Created area code specific subfolders within raw data directory - Altered timestamp adjustments to cover entire days - Used config.DATA_RAW_DIR for dynamic raw data directory path

--- src/data_fetcher.py
+++ src/data_fetcher.py
@@ -6,6 +6,9 @@ import os
 from entsoe import EntsoeRawClient
 import pandas as pd
 from dotenv import load_dotenv
+import time
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import config
 
 # Load environment variables from .env
 load_dotenv()
@@ -22,20 +25,7 @@ area_codes = {
     "NO5": "10Y1001A1001A48H"   # NO5 Western Norway
 }
 
-
 client = EntsoeRawClient(api_key=api_key)
-
-def align_to_monday(date):
-    """
-    Adjusts a given date to the previous Monday if it's not already a Monday.
-
-    Parameters:
-    date (pd.Timestamp): The date to align.
-
-    Returns:
-    pd.Timestamp: The aligned date.
-    """
-    return date - pd.DateOffset(days=date.weekday())
 
 def fetch_data(start_date, end_date, area_code_name):
     country_code = area_codes.get(area_code_name)
@@ -43,32 +33,55 @@ def fetch_data(start_date, end_date, area_code_name):
         print(f"Invalid area code name: {area_code_name}")
         return
 
-    start = pd.Timestamp(start_date, tz='Europe/Oslo')
-    end = pd.Timestamp(end_date, tz='Europe/Oslo')
-    current = align_to_monday(start)
+    # Adjust start and end times to cover the entire days
+    start = pd.Timestamp(start_date, tz='Europe/Oslo').replace(hour=0, minute=0, second=0)
+    end = pd.Timestamp(end_date, tz='Europe/Oslo') + pd.DateOffset(days=1)
+
+    max_days_per_request = 370
+    current = start
+
+    # Create subfolder for area code if it doesn't exist
+    area_code_folder = os.path.join(config.DATA_RAW_DIR, area_code_name)
+    os.makedirs(area_code_folder, exist_ok=True)
 
     while current < end:
-        # Set the end of the week to the nearest Sunday, including all of Sunday
-        week_end = min(current + pd.DateOffset(days=6, hours=23, minutes=59, seconds=59), end)
-        print(f"Fetching data from {current} to {week_end}...")
+        interval_end = min(current + pd.DateOffset(days=max_days_per_request - 1), end)
+        print(f"Fetching data for {area_code_name}: {current} to {interval_end}...")
 
-        xml_string = client.query_day_ahead_prices(country_code, current, week_end)
+        try:
+            xml_string = client.query_day_ahead_prices(country_code, current, interval_end)
+            filename = f"{area_code_name}_{current.strftime('%Y_%m_%d')}_to_{interval_end.strftime('%Y_%m_%d')}_day_ahead_prices.xml"
+            output_file_path = os.path.join(area_code_folder, filename)
 
-        # Save each week's data in a separate file
-        raw_data_directory = os.path.join('data', 'raw')
-        os.makedirs(raw_data_directory, exist_ok=True)
+            with open(output_file_path, 'w') as f:
+                f.write(xml_string)
 
-        # Format the filename with the year, month, and week number
-        filename = f"{area_code_name}_Week{current.strftime('%W')}_{current.strftime('%Y_%m_%d')}_to_{week_end.strftime('%Y_%m_%d')}_prices.xml"
-        output_file_path = os.path.join(raw_data_directory, filename)
+            print(f"Data saved to {output_file_path}")
 
-        with open(output_file_path, 'w') as f:
-            f.write(xml_string)
+        except Exception as e:
+            print(f"Failed to fetch data: {e}")
 
-        print(f"Data saved to {output_file_path}")
-        # Move to the next Monday
-        current = week_end + pd.DateOffset(minutes=1) if week_end < end else end
+        # Set current to the day after interval_end
+        current = interval_end + pd.DateOffset(minutes=1)
+
+def fetch_data_with_retries(start_date, end_date, area_code_name, max_retries=5, delay=10):
+    attempts = 0
+    while attempts < max_retries:
+        try:
+            fetch_data(start_date, end_date, area_code_name)
+            return  # Successful fetch, exit function
+        except (ConnectionError, Timeout, TooManyRedirects) as e:
+            attempts += 1
+            print(f"Attempt {attempts}/{max_retries} failed: {e}. Retrying in {delay} seconds...")
+            time.sleep(delay)
+
+    print("Max retries reached. Exiting.")
 
 if __name__ == "__main__":
-    # Example usage
-    fetch_data('20231002', '20231102', 'NO1')
+    """
+    A single request can only contain 370 doucments per request due to the limitations on API.
+    Therefore when querying dates/periods longer than one year(roughly) the code will split
+    the dates into an appropriate amount of requests.
+    Example usage is fetch_data_with_retries(StartDate(YYYYMMDD), EndDate(YYYYMMDD), Area_code(NO#))
+    """
+    fetch_data_with_retries('20170101', '20221231', 'NO1')
