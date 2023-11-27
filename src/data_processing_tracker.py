a2620968ee0ef88b7e285f24d98a63a61ba8e93c
Haakon Webb <haakonwebb@gmail.com>
2023-11-27 19:15:14 +0900

    Removed lint.

--- src/data_processing_tracker.py
+++ src/data_processing_tracker.py
@@ -2,7 +2,6 @@
 # This file is part of NEPP which is released under GNU GPLv3.
 # See file LICENSE or go to https://www.gnu.org/licenses/gpl-3.0.html for full license details.
 
-
 def update_file_metadata(file_path, new_stage):
     """
     Update the metadata of a file to include a new processing stage.
