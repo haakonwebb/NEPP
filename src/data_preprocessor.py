6e4e19dc06c5a04a2ddbed1cce72df4343d355fb
Haakon Webb <haakonwebb@gmail.com>
2023-11-30 13:00:35 +0900

    Made major changes to processing files.
    Removed functionality of passing dates through functions.
    
    Script now instead filters files by the year they belong to.
    
    Script changed to convert to UTC+1 instead of previously trying to convert to UTC.
    
    Script will now correctly check for XML files in the area code folders of raw data and sort the ones that belong to their respective years and append correctly to their CSV files and sort them in ascending order.

--- src/data_preprocessor.py
+++ src/data_preprocessor.py
@@ -10,7 +10,7 @@ import pandas as pd
 import glob
 import random
 from data_processing_tracker import update_file_metadata
-from utils import save_df_to_csv
+import pytz
 
 namespace_cache = {}  # Cache for storing namespaces
 
@@ -39,30 +39,40 @@ def check_area_code_folder(area_code):
     folder_path = os.path.join(config.DATA_RAW_DIR, area_code)
     return os.path.exists(folder_path)
 
-def parse_xml_to_df(xml_file_path, user_start_date, user_end_date):
+def parse_xml_to_df(xml_file_path):
     """
     Parse the XML file containing electricity price data and convert it to a pandas DataFrame.
 
     Parameters:
     xml_file_path (str): The path to the XML file containing the data.
-    start_date (str): The start date in 'YYYY-MM-DD' format.
-    end_date (str): The end date in 'YYYY-MM-DD' format.
 
     Returns:
     DataFrame: The converted data as a pandas DataFrame.
     """
 
     area_code = os.path.basename(xml_file_path).split('_')[0]
+    # Assuming get_namespace is a function defined elsewhere in your code
     ns = get_namespace(area_code, xml_file_path)
+
+    # Extracting start and end dates from the file name
+    file_name_parts = os.path.basename(xml_file_path).split('_')
+    file_start_date = ''.join(file_name_parts[1:4])  # Joins year, month, day
+    file_end_date = ''.join(file_name_parts[5:8])   # Joins year, month, day
 
     tree = ET.parse(xml_file_path)
     root = tree.getroot()
 
     data_records = []
+    
+    # Define UTC and UTC+1 timezones
+    utc_plus_1 = pytz.FixedOffset(60)  # 60 minutes offset for UTC+1
 
-    # Remember to change the timezones if necessary
-    start_date = pd.Timestamp(user_start_date).tz_localize('UTC')
-    end_date = pd.Timestamp(user_end_date).tz_localize('UTC') + pd.Timedelta(days=1) - pd.Timedelta(seconds=1)  # Include last measurement of end_date
+    # Convert input dates to offset-aware UTC+1
+    start_date = datetime.strptime(file_start_date, '%Y%m%d')
+    end_date = datetime.strptime(file_end_date, '%Y%m%d') + timedelta(days=1) - timedelta(seconds=1)
+
+    start_date_utc1 = utc_plus_1.localize(start_date)
+    end_date_utc1 = utc_plus_1.localize(end_date)
 
     for timeseries in root.findall('.//ns:TimeSeries', ns):
         business_type = timeseries.find('ns:businessType', ns).text
@@ -71,22 +81,27 @@ def parse_xml_to_df(xml_file_path, user_start_date, user_end_date):
         currency = timeseries.find('ns:currency_Unit.name', ns).text
 
         for period in timeseries.findall('.//ns:Period', ns):
-            period_start = datetime.fromisoformat(period.find('.//ns:timeInterval/ns:start', ns).text)
-            period_end = datetime.fromisoformat(period.find('.//ns:timeInterval/ns:end', ns).text)
+            period_start_str = period.find('.//ns:timeInterval/ns:start', ns).text
             
+            # Convert the string to a datetime object
+            period_start = datetime.fromisoformat(period_start_str)
+
+            # Localize to UTC if tzinfo is None
+            if period_start.tzinfo is None:
+                period_start = pytz.utc.localize(period_start)
+
             for point in period.findall('.//ns:Point', ns):
                 position = int(point.find('ns:position', ns).text)
                 price_amount = float(point.find('ns:price.amount', ns).text)
 
                 hour_adjustment = timedelta(hours=position - 1)
-                measurement_start_time = pd.Timestamp(period_start + hour_adjustment).tz_convert('UTC')
 
-                # Ensure 'measurement_start_time' is within the period range
-                if not (period_start <= measurement_start_time < period_end):
-                    continue
+                # Convert the measurement start time to UTC+1
+                measurement_start_time_utc1 = period_start + hour_adjustment
+                measurement_start_time_utc1 = measurement_start_time_utc1.astimezone(pytz.FixedOffset(60))
 
-                # Skip data points outside the specified date range
-                if measurement_start_time < start_date or measurement_start_time > end_date:
+                # Check if measurement_start_time is within the specified date range
+                if not (measurement_start_time_utc1 >= start_date_utc1 and measurement_start_time_utc1 <= end_date_utc1):
                     continue
 
                 data_records.append({
@@ -95,37 +110,47 @@ def parse_xml_to_df(xml_file_path, user_start_date, user_end_date):
                     'in_domain': in_domain,
                     'out_domain': out_domain,
                     'currency': currency,
-                    'period_start': measurement_start_time.isoformat(),
-                    'period_end': (measurement_start_time + timedelta(hours=1)).isoformat()
+                    'period_start': measurement_start_time_utc1.isoformat(),
+                    'period_end': (measurement_start_time_utc1 + timedelta(hours=1)).isoformat()
                 })
 
     df = pd.DataFrame(data_records)
 
-    # Ensure 'period_start' is in datetime format
-    df['period_start'] = pd.to_datetime(df['period_start'])
+    # Only convert if 'period_start' column exists
+    if 'period_start' in df.columns:
+        df['period_start'] = pd.to_datetime(df['period_start'])
+        df['period_end'] = pd.to_datetime(df['period_end'])
 
     return df
 
-def process_files(file_list, user_start_date, user_end_date):
+def process_files(file_list):
     for xml_file in file_list:
-        df = parse_xml_to_df(xml_file, user_start_date, user_end_date)
+        df = parse_xml_to_df(xml_file)
 
-        # Group by year and save each group in a separate file
         for year, year_df in df.groupby(df['period_start'].dt.year):
             area_code = os.path.basename(xml_file).split('_')[0]
             area_code_folder = os.path.join(config.DATA_PREPROCESSED_DIR, area_code)
             os.makedirs(area_code_folder, exist_ok=True)
 
-            # File naming based on area code and year
             file_name = f"{area_code}_{year}.csv"
             preprocessed_file_path = os.path.join(area_code_folder, file_name)
 
-            # Check if the file already exists to determine append or create a new one
             if os.path.exists(preprocessed_file_path):
-                # Append without header
-                year_df.to_csv(preprocessed_file_path, mode='a', index=False, header=False)
+                existing_df = pd.read_csv(preprocessed_file_path)
+
+                # Convert 'period_start' to datetime for both DataFrames
+                existing_df['period_start'] = pd.to_datetime(existing_df['period_start'])
+                year_df['period_start'] = pd.to_datetime(year_df['period_start'])
+
+                # Combine the data
+                combined_df = pd.concat([existing_df, year_df])
+
+                # Drop duplicates and sort the data in descending order
+                combined_df.drop_duplicates(subset=['period_start'], inplace=True)
+                combined_df.sort_values(by='period_start', inplace=True)
+
+                combined_df.to_csv(preprocessed_file_path, index=False)
             else:
-                # Create a new file with header
                 year_df.to_csv(preprocessed_file_path, index=False)
 
             try:
@@ -134,77 +159,62 @@ def process_files(file_list, user_start_date, user_end_date):
             except Exception as e:
                 print(f"Error updating metadata for {preprocessed_file_path}: {e}")
 
-def process_all_xml_files(area_code, user_start_date, user_end_date):
+def filter_xml_files_by_year(area_code, start_year, end_year):
     folder_path = os.path.join(config.DATA_RAW_DIR, area_code)
-    xml_files = glob.glob(os.path.join(folder_path, '*.xml'))
-    process_files(xml_files, user_start_date, user_end_date)
+    all_xml_files = glob.glob(os.path.join(folder_path, '*.xml'))
 
-def process_first_x_files(area_code, x, user_start_date, user_end_date):
-    folder_path = os.path.join(config.DATA_RAW_DIR, area_code)
-    xml_files = sorted(glob.glob(os.path.join(folder_path, '*.xml')))[:x]
-    process_files(xml_files, user_start_date, user_end_date)
+    filtered_files = []
+    for file in all_xml_files:
+        # Extract the year information from the file name
+        file_name_parts = os.path.basename(file).split('_')
+        file_start_year = int(file_name_parts[1])
+        file_end_year = int(file_name_parts[5])
 
-def process_last_x_files(area_code, x, user_start_date, user_end_date):
-    folder_path = os.path.join(config.DATA_RAW_DIR, area_code)
-    xml_files = sorted(glob.glob(os.path.join(folder_path, '*.xml')))[-x:]
-    process_files(xml_files, user_start_date, user_end_date)
+        # Check if the file's year range overlaps with the user-specified range
+        if file_end_year >= start_year and file_start_year <= end_year:
+            filtered_files.append(file)
 
-def process_random_x_files(area_code, x, user_start_date, user_end_date):
-    folder_path = os.path.join(config.DATA_RAW_DIR, area_code)
-    xml_files = glob.glob(os.path.join(folder_path, '*.xml'))
-    
-    if len(xml_files) < x:
-        selected_files = xml_files
-    else:
-        selected_files = random.sample(xml_files, x)
-
-    process_files(selected_files, user_start_date, user_end_date)
+    return filtered_files
 
 if __name__ == "__main__":
     
     # Example usage for specific files
-    # xml_file_path = 'NO1_Week40_2023_10_02_to_2023_10_08_day_ahead_prices.xml'  # Replace with your XML file path
+    # xml_file_path = 'NO1_2023_10_02_to_2023_10_08_day_ahead_prices.xml'  # Replace with your XML file path
     
     area_code = input("Enter the area code (e.g., 'NO1'): ").strip().upper()
-    
+
     if not check_area_code_folder(area_code):
         print(f"No data folder found for area code {area_code}. Exiting.")
         exit()
 
-    user_start_date = input("Enter the start date (YYYY-MM-DD): ").strip()
-    user_end_date = input("Enter the end date (YYYY-MM-DD): ").strip()
+    start_year = int(input("Enter the start year (YYYY): "))
+    end_year = int(input("Enter the end year (YYYY): "))
 
-    user_choice = input("Choose an option: 'specific', 'all', 'first X', 'last X', or 'random X' files: ").strip().lower()
+    user_choice = input("Choose an option: 'all', 'first X', 'last X', or 'random X' files: ").strip().lower()
 
-    if user_choice == 'specific':
-        file_name = input("Enter the name of the file to process (e.g., 'example.xml'): ").strip()
-        xml_file_path = os.path.join(config.DATA_RAW_DIR, area_code, file_name)
+    # Filter XML files once here
+    xml_files = filter_xml_files_by_year(area_code, start_year, end_year)
 
-        if os.path.exists(xml_file_path):
-            processed_data_df = parse_xml_to_df(xml_file_path, user_start_date, user_end_date)
-            csv_filename = file_name.replace('.xml', '.csv')
-            save_df_to_csv(processed_data_df, csv_filename)
-            print(f"Processed file saved as: {csv_filename}")
-        else:
-            print(f"File not found: {xml_file_path}")
-
-    elif user_choice == 'all':
-        process_all_xml_files(area_code, user_start_date, user_end_date)
-        print("Processed all files in the specified area code directory.")
+    if user_choice == 'all':
+        process_files(xml_files)
+        print("Processed all relevant files in the specified area code directory.")
 
     elif user_choice.startswith('first'):
         num_files = int(user_choice.split()[1])
-        process_first_x_files(area_code, num_files, user_start_date, user_end_date)
+        first_x_files = sorted(xml_files)[:num_files]
+        process_files(first_x_files)
         print(f"Processed the first {num_files} files in the specified area code directory.")
 
     elif user_choice.startswith('last'):
         num_files = int(user_choice.split()[1])
-        process_last_x_files(area_code, num_files, user_start_date, user_end_date)
+        last_x_files = sorted(xml_files)[-num_files:]
+        process_files(last_x_files)
         print(f"Processed the last {num_files} files in the specified area code directory.")
 
     elif user_choice.startswith('random'):
         num_files = int(user_choice.split()[1])
-        process_random_x_files(area_code, num_files, user_start_date, user_end_date)
+        random_x_files = random.sample(xml_files, min(num_files, len(xml_files)))
+        process_files(random_x_files)
         print(f"Processed a random selection of {num_files} files from the specified area code directory.")
 
     else:
